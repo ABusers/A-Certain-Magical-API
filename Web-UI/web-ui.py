b45854fdf217fdaedc7ed6063d31de18e2f6ca61
Trevor Bergeron <mal@sec.gd>
2016-03-01 20:35:24 -0500

    Fix webui after api change

--- Web-UI/web-ui.py
+++ Web-UI/web-ui.py
@@ -43,22 +43,22 @@ def show(asset_id, subdub):
         eps = [x for x in f.get_videos(int(asset_id)) if x.dub_sub.lower() == subdub]  # Hacky, needs architecture fix
     except AttributeError:  # FIXME: This is not the correct way to do this.
         return render_template('message.html', message="API Error: Does this show have any episodes?")
-    return render_template('episodes.html',title=show_title, eps=eps, show_id=asset_id, subdub=subdub)
+    return render_template('episodes.html', title=show_title, eps=eps, show_id=asset_id, subdub=subdub)
 
 
-@app.route('/show/<int:asset_id>/{sub,dub}/<int:episode_id>/play', defaults={'filename': None})
-@app.route('/show/<int:asset_id>/{sub,dub}/<int:episode_id>/play/<filename>')
+@app.route('/show/<int:asset_id>/<int:episode_id>/play', defaults={'filename': None})
+@app.route('/show/<int:asset_id>/<int:episode_id>/play/<filename>')
 def play_episode(asset_id, episode_id, filename):
     # Discard filename, that's just to make the browser happy.
 
     # Gross, needs architecture fix
     episode = None
-    for ep in f.get_videos(int(asset_id)):
-        if ep.asset_id == episode_id:
+    for ep in f.get_videos(asset_id):
+        if int(ep.asset_id) == episode_id:
             episode = ep
             break
 
-    playlist_url = episode.video_url # , episode.quality)
+    playlist_url = episode.video_url  # , episode.quality)
 
     response = make_response("#EXTM3U\n"+playlist_url)
     response.headers['Content-Type'] = 'application/vnd.apple.mpegurl'
